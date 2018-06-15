94eae748d8fdc62d26e820627047578e72834b7d
Mariam Chaudry <mariamschaudry1@gmail.com>
2018-06-15 15:14:52 -0400

    update

--- models/Article.js
+++ models/Article.js
@@ -9,12 +9,14 @@ var ArticleSchema = new Schema({
     // "title" is required and of type String // 
     title: {
         type: String,
-        required: true
+        required: true,
+        unique: true
     },
     // "link" is required and of type String //
     link: {
         type: String,
-        required: true
+        required: true,
+        unique: true
     },
     // "summary" is required and of type String //
     summary: {
@@ -25,7 +27,7 @@ var ArticleSchema = new Schema({
     // The ref property links the ObjectId to the Note model //
     // This allows us to populate the Article with an associated Note //
     note: {
-        tyoe: Schema.Types.ObjectId,
+        type: Schema.Types.ObjectId,
         ref: "Note"
     }
 });
